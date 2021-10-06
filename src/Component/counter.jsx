f32612c44c53be8bb71bb026aea5f324ef9e3d94
anurag99x <anuragxpandey@gmail.com>
2021-10-06 20:37:05 +0530

    first

---
+++ src/Component/counter.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useState } from "react";
+import { addCounter, reduceCounter } from "../Redux/action";
+import { AppContext } from "../Redux/appContextProvider";
+import { store } from "../Redux/store";
+const Counter = () => {
+  const [state, setState] = useState(0);
+
+  const { dispatch, getState } = React.useContext(AppContext);
+  const { counter } = getState();
+  const forceUpdate = () => setState(state + 1);
+
+  const handleAdd = () => {
+    dispatch(addCounter(1));
+    forceUpdate();
+  };
+
+  const handleReduce = () => {
+    dispatch(reduceCounter(1));
+    forceUpdate();
+  };
+  return (
+    <>
+      <h1>Counter : {counter}</h1>
+      <button onClick={handleAdd}>ADD</button>
+      <button onClick={handleReduce}>REDUCE</button>
+    </>
+  );
+};
+
+export { Counter };
